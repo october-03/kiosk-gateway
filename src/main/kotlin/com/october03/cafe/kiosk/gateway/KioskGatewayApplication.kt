f15615e2940391d9373fd59fc5a4f26a5a36d83a
october03 <season_autumn@kakao.com>
2024-02-09 17:14:59 +0900

    사용하지 않는 import 코드 제거

--- src/main/kotlin/com/october03/cafe/kiosk/gateway/KioskGatewayApplication.kt
+++ src/main/kotlin/com/october03/cafe/kiosk/gateway/KioskGatewayApplication.kt
@@ -2,10 +2,6 @@ package com.october03.cafe.kiosk.gateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.gateway.route.RouteLocator
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
-import org.springframework.cloud.gateway.route.builder.routes
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class KioskGatewayApplication
